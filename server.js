b8f82da126f3ec35686adc853703402070d08f85
fabioferrero90 <fabio.ff90@gmail.com>
2024-12-18 02:52:29 +0100

    feat: add /bacheca and public imgs

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const posts = require('./data/posts')
 
 const port = 3000;
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.send("Server del mio Blog")
 })
@@ -13,3 +15,9 @@ app.listen(port, () => {
   console.log(`Sono in ascolto alla porta ${port}`)
 })
 
+app.get('/bacheca', (req, res) => {
+  posts.postcount = 0;
+  posts.forEach(el => posts.postcount++)
+  console.log(posts)
+  res.json(posts)
+})
